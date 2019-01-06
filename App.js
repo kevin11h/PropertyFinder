55aecb61abb24f105ef639b3fb3cd7620babd2f5
kevin11h <kevhung11@gmail.com>
2019-01-05 20:46:05 -0800

    added navigation bar

--- App.js
+++ App.js
@@ -10,6 +10,11 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View } from 'react-native';
 'use strict';
 
+import {
+	createStackNavigator,
+	createAppContainer
+} from 'react-navigation';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -18,33 +23,35 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+// export default class App extends Component<Props> {
+//   render() {
+//     return React.createElement(Text, {style: styles.description},
+//     	"Search for houses to buy!");
+//   }
+// }
+
+class SearchPage extends Component<Props> {
+	static navigationOptions = {
+		title: 'Property Finder',
+	};
+
+	render() {
+    	return React.createElement(Text, {style: styles.description},
+    		"Search for houses to buy!");
+  	}
 }
 
+const App = createStackNavigator({
+		Home: { screen: SearchPage },
+});
+
+export default createAppContainer(App);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  description: {
+  	fontSize: 18,
+  	textAlign: 'center',
+  	color: '#656565',
+  	marginTop: 65,
+  }
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+`npm install react-native-gesture-handler`
+
+`react-native link react-native-gesture-handler`
+
+`npm installr react-navigation`
+
+`react-native run-android`
+
+`yarn add react-navigation
